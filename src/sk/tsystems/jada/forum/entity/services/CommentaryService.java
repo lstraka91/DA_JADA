4aac35c107fca5037319663a3d0936501402c5c8
tado5 <tadeasscesnak@gmail.com>
2016-08-03 15:34:41 +0200

    Commentary Service vol.1,75

--- src/sk/tsystems/jada/forum/entity/services/CommentaryService.java
+++ src/sk/tsystems/jada/forum/entity/services/CommentaryService.java
@@ -4,72 +4,62 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 
-
 import sk.tsystems.jada.forum.entity.Commentary;
-
-
 
 public class CommentaryService {
 
-	
 	public void addComent(Commentary comment) {
 		JpaHelper.beginTransaction();
 		EntityManager em = JpaHelper.getEntityManager();
 		em.persist(comment);
 		JpaHelper.commitTransaction();
 	}
-	
-	public void removeCommentById(int id){
+
+	public void removeCommentById(int id) {
 		Commentary comment = new Commentary();
 		JpaHelper.beginTransaction();
 		EntityManager em = JpaHelper.getEntityManager();
-		comment=em.find(Commentary.class,id);
+		comment = em.find(Commentary.class, id);
 		em.remove(comment);
 		JpaHelper.commitTransaction();
 	}
-	
-	public void removeCommentByObject(Commentary comment){
+
+	public void removeCommentByObject(Commentary comment) {
 		JpaHelper.beginTransaction();
 		EntityManager em = JpaHelper.getEntityManager();
-		comment=em.find(Commentary.class,comment);
+		comment = em.find(Commentary.class, comment);
 		em.remove(comment);
 		JpaHelper.commitTransaction();
 	}
-	
-	
+
 	public List<Commentary> selectAllComent() {
-		
-		
-			JpaHelper.beginTransaction();
-			EntityManager em = JpaHelper.getEntityManager();
-			JpaHelper.commitTransaction();
-			List<Commentary> list  = em.createQuery("SELECT c FROM CommentJPA c where c.game.gameID = '' ").getResultList();
 
-			return list;
-		}
-	
+		JpaHelper.beginTransaction();
+		EntityManager em = JpaHelper.getEntityManager();
+		JpaHelper.commitTransaction();
+		List<Commentary> list = em.createQuery("SELECT c FROM CommentJPA c where c.game.gameID = '' ").getResultList();
+
+		return list;
+	}
+
 	public List<Commentary> selectAllComentByTopic() {
-		
-		
+
 		JpaHelper.beginTransaction();
 		EntityManager em = JpaHelper.getEntityManager();
 		JpaHelper.commitTransaction();
-		List<Commentary> list  = em.createQuery("SELECT c FROM CommentJPA c where c.game.gameID = '' ").getResultList();
+		List<Commentary> list = em.createQuery("SELECT c FROM CommentJPA c where c.game.gameID = '' ").getResultList();
 
 		return list;
 	}
-	
+
 	public List<Commentary> selectAllComentByPerson() {
-		
-		
+
 		JpaHelper.beginTransaction();
 		EntityManager em = JpaHelper.getEntityManager();
 		JpaHelper.commitTransaction();
-		List<Commentary> list  = em.createQuery("SELECT c FROM CommentJPA c where c.game.gameID = '' ").getResultList();
+		List<Commentary> list = em.createQuery("SELECT c FROM CommentJPA c where c.game.gameID = '' ").getResultList();
 
 		return list;
 	}
-	
-	
-	
+
 }
