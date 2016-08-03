71237784fe362babe147bd955c3f39afef643a1d
popadicbranislav <popadicbranislav@gmail.com>
2016-08-03 13:31:23 +0200

    PersonService update
    updated methods for person insertion, getting person by name or id,
    register new player with check.

--- src/sk/tsystems/jada/forum/entity/services/PersonService.java
+++ src/sk/tsystems/jada/forum/entity/services/PersonService.java
@@ -1,3 +1,6 @@
+/**
+ * @author Branislav Popadiè
+ */
 package sk.tsystems.jada.forum.entity.services;
 
 import javax.persistence.EntityManager;
@@ -8,7 +11,8 @@ import sk.tsystems.jada.forum.entity.Person;
 public class PersonService {
 
 	/**
-	 * 
+	 * Find object of Class Person in database by persons ID.
+	 *
 	 * @param idPerson
 	 * @return Object Person with parameter idPerson.
 	 */
@@ -17,53 +21,57 @@ public class PersonService {
 		return em.find(Person.class, idPerson);
 	}
 
+	/**
+	 * Find object of Class Person in database by persons name.
+	 * 
+	 * @param personName
+	 * @return Object Person with requested personName or null when person not
+	 *         in database.
+	 */
 	private Person getPersonByName(String personName) {
-		int id = 0;
+		Person person;
 		EntityManager em = JpaHelper.getEntityManager();
-		Query query = em.createQuery("select idPerson from Person p where p.personName = :personName");
+		Query query = em.createQuery("select p from Person p where p.personName = :personName");
 
 		query.setParameter("personName", personName);
 		if (!query.getResultList().isEmpty()) {
-			id = (int) query.getResultList().get(0);
-			return getPerson(id);
+			person = (Person) query.getResultList().get(0);
+			return person;
 		} else {
 			return null;
 		}
 	}
 
-//	public Person getPersonWithCheck(String personName) {
-		// int
-//	}
+	/**
+	 * Checks if player is already in database or register new player in
+	 * database.
+	 * 
+	 * @param person
+	 * @return returns null if person already is in database otherwise persists
+	 *         Player in database and returns Player.
+	 */
+	public Person registerPerson(Person person) {
 
-	// public Player checkPlayer(String playerName) {
-	// int idPlayer = playerId(playerName);
-	// System.out.println(idPlayer);
-	// if (idPlayer != 0) {
-	// return getPlayer(idPlayer);
-	// } else {
-	// Player playerRes = new Player(playerName);
-	// insertPlayer(playerRes);
-	// return playerRes;
-	// }
-	// }
-	//
-	// private int playerId(String playerName) {
-	// int id = 0;
-	// EntityManager em = JpaHelper.getEntityManager();
-	// Query query = em.createQuery("select idPlayer from Player p where
-	// p.playerName = :name");
-	// query.setParameter("name", playerName);
-	// if (!query.getResultList().isEmpty()) {
-	// id = (int) query.getResultList().get(0);
-	// }
-	// return id;
-	// }
-	//
-	// private void insertPlayer(Player player) {
-	// JpaHelper.beginTransaction();
-	// EntityManager em = JpaHelper.getEntityManager();
-	// em.persist(player);
-	// JpaHelper.commitTransaction();
-	// }
+		int idPerson = getPersonByName(person.getPersonName()).getIdPerson();
+		System.out.println(idPerson);
+		if (idPerson != 0) {
+			return null;
+		} else {
+			insertPerson(person);
+			return person;
+		}
+	}
+
+	/**
+	 * Persists new object of Person into the database.
+	 * 
+	 * @param person
+	 */
+	private void insertPerson(Person person) {
+		JpaHelper.beginTransaction();
+		EntityManager em = JpaHelper.getEntityManager();
+		em.persist(person);
+		JpaHelper.commitTransaction();
+	}
 
 }
