4a6d68978ad25318e2bf02bfe2cffa7664a3e2f5
Lukas Straka <lstraka91@gmail.com>
2016-08-03 11:16:21 +0200

    added entity Rating and RatingId with composite Primary keys

---
+++ src/sk/tsystems/jada/forum/entity/RatingId.java
@@ -0,0 +1,63 @@
+package sk.tsystems.jada.forum.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class RatingId implements Serializable{
+
+	/**
+	 * auto generated serialUID
+	 */
+	private static final long serialVersionUID = -5416666417570281293L;
+
+	/**
+	 * Object that indicate comment to which was rating applied
+	 */
+	private Commentary comment;
+	
+	/**
+	 * Object that indicate user who rate the comment
+	 */
+	private User user;
+
+	/**
+	 * getter method that returns Commentary object
+	 * 
+	 * @return comment
+	 */
+	
+	public Commentary getComment() {
+		return comment;
+	}
+
+	/**
+	 * set the comment object
+	 * 
+	 * @param comment
+	 */
+	public void setComment(Commentary comment) {
+		this.comment = comment;
+	}
+
+	/**
+	 * getter method that return User object
+	 * 
+	 * @return user
+	 */
+	
+	public User getUser() {
+		return user;
+	}
+
+	/**
+	 * set the user object
+	 * 
+	 * @param user
+	 */
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
