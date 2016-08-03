30c13a3c1aa86de643332ebb4dbf892e51d79fbc
tado5 <tadeasscesnak@gmail.com>
2016-08-03 15:41:25 +0200

    Commentary service vol.1,83

--- src/sk/tsystems/jada/forum/entity/services/CommentaryService.java
+++ src/sk/tsystems/jada/forum/entity/services/CommentaryService.java
@@ -8,6 +8,11 @@ import sk.tsystems.jada.forum.entity.Commentary;
 
 public class CommentaryService {
 
+	/**
+	 * Insert Comment into DB
+	 * 
+	 * @param comment
+	 */
 	public void addComent(Commentary comment) {
 		JpaHelper.beginTransaction();
 		EntityManager em = JpaHelper.getEntityManager();
@@ -15,23 +20,11 @@ public class CommentaryService {
 		JpaHelper.commitTransaction();
 	}
 
-	public void removeCommentById(int id) {
-		Commentary comment = new Commentary();
-		JpaHelper.beginTransaction();
-		EntityManager em = JpaHelper.getEntityManager();
-		comment = em.find(Commentary.class, id);
-		em.remove(comment);
-		JpaHelper.commitTransaction();
-	}
-
-	public void removeCommentByObject(Commentary comment) {
-		JpaHelper.beginTransaction();
-		EntityManager em = JpaHelper.getEntityManager();
-		comment = em.find(Commentary.class, comment);
-		em.remove(comment);
-		JpaHelper.commitTransaction();
-	}
-
+	/**
+	 * Select all comment from DB
+	 * 
+	 * @return List<Commentary>
+	 */
 	public List<Commentary> selectAllComent() {
 
 		JpaHelper.beginTransaction();
@@ -42,6 +35,11 @@ public class CommentaryService {
 		return list;
 	}
 
+	/**
+	 * sSelect comment by Topic
+	 * 
+	 * @return List<Commentary>
+	 */
 	public List<Commentary> selectAllComentByTopic() {
 
 		JpaHelper.beginTransaction();
@@ -52,6 +50,11 @@ public class CommentaryService {
 		return list;
 	}
 
+	/**
+	 * Select comment by Person
+	 * 
+	 * @return List<Commentary>
+	 */
 	public List<Commentary> selectAllComentByPerson() {
 
 		JpaHelper.beginTransaction();
@@ -60,6 +63,33 @@ public class CommentaryService {
 		List<Commentary> list = em.createQuery("SELECT c FROM CommentJPA c where c.game.gameID = '' ").getResultList();
 
 		return list;
+	}
+
+	/**
+	 * Remove comment by ID
+	 * 
+	 * @param id
+	 */
+	public void removeCommentById(int id) {
+		Commentary comment = new Commentary();
+		JpaHelper.beginTransaction();
+		EntityManager em = JpaHelper.getEntityManager();
+		comment = em.find(Commentary.class, id);
+		em.remove(comment);
+		JpaHelper.commitTransaction();
+	}
+
+	/**
+	 * Remove comment by object
+	 * 
+	 * @param comment
+	 */
+	public void removeCommentByObject(Commentary comment) {
+		JpaHelper.beginTransaction();
+		EntityManager em = JpaHelper.getEntityManager();
+		comment = em.find(Commentary.class, comment);
+		em.remove(comment);
+		JpaHelper.commitTransaction();
 	}
 
 }
