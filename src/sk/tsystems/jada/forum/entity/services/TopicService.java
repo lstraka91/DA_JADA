0d86ea5f98f5fec860d92d3424c2d82c660faaf6
Jozef <Jozef@192.168.1.227>
2016-08-03 13:32:52 +0200

    topic service

--- src/sk/tsystems/jada/forum/entity/services/TopicService.java
+++ src/sk/tsystems/jada/forum/entity/services/TopicService.java
@@ -11,37 +11,73 @@ public class TopicService {
 	 * 
 	 * @param topic
 	 */
-	public void addTopic(Topic topic){
+	public void addTopic(Topic topic) {
 		JpaHelper.beginTransaction();
 		EntityManager em = JpaHelper.getEntityManager();
 		em.persist(topic);
 		JpaHelper.commitTransaction();
 	}
-	
+
 	/**
 	 * Method for remove topic from database by object topic
 	 * 
 	 * @param topic
 	 */
-	public void removeTopicByObject(Topic topic){
+	public void removeTopicByObject(Topic topic) {
 		JpaHelper.beginTransaction();
 		EntityManager em = JpaHelper.getEntityManager();
 		topic = em.find(Topic.class, topic);
 		em.remove(topic);
 		JpaHelper.commitTransaction();
 	}
-	
+
 	/**
 	 * Method for remove topic from database by id topic
 	 * 
 	 * @param id
 	 */
-	public void removeTopicById(int id){
+	public void removeTopicById(int id) {
 		Topic topic = new Topic();
 		JpaHelper.beginTransaction();
 		EntityManager em = JpaHelper.getEntityManager();
 		topic = em.find(Topic.class, id);
 		em.remove(topic);
 		JpaHelper.commitTransaction();
+	}
+
+	/**
+	 * Method for update topic name
+	 * 
+	 * @param topic
+	 */
+	public void updateTopicName(Topic topic, String topicName) {
+		JpaHelper.beginTransaction();
+		EntityManager em = JpaHelper.getEntityManager();
+		topic = em.find(Topic.class, topic);
+		topic.setTopicName(topicName);
+		JpaHelper.commitTransaction();
+	}
+
+	/**
+	 * Method for update topic description
+	 * 
+	 * @param topic
+	 */
+	public void updateTopicDescrition(Topic topic, String topicDescription) {
+		JpaHelper.beginTransaction();
+		EntityManager em = JpaHelper.getEntityManager();
+		topic = em.find(Topic.class, topic);
+		topic.setTopicDescription(topicDescription);
+		JpaHelper.commitTransaction();
+	}
+
+	/**
+	 * Method for select all topics from database
+	 * 
+	 * @return
+	 */
+	public Topic findTopics() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
