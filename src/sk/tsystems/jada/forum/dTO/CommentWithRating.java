d9e2b6a8a4a242059321e1429a7a58ca35aa6e55
Lukas Straka <lstraka91@gmail.com>
2016-08-09 17:04:16 +0200

    added new classes to getRatings for comments (in PROGRESS)

---
+++ src/sk/tsystems/jada/forum/dTO/CommentWithRating.java
@@ -0,0 +1,46 @@
+package sk.tsystems.jada.forum.dTO;
+
+import sk.tsystems.jada.forum.entity.Commentary;
+import sk.tsystems.jada.forum.entity.Rating;
+
+public class CommentWithRating {
+
+	private Commentary comment;
+	private Rating rating;
+	private int rateOfComment;
+	
+	public CommentWithRating(Commentary comment, Rating rating, int rateOfComment) {
+		this.comment = comment;
+		this.rating = rating;
+		this.rateOfComment = rateOfComment;
+	}
+
+	public Commentary getComment() {
+		return comment;
+	}
+
+	public void setComment(Commentary comment) {
+		this.comment = comment;
+	}
+
+	public Rating getRating() {
+		return rating;
+	}
+
+	public void setRating(Rating rating) {
+		this.rating = rating;
+	}
+
+	public int getRateOfComment() {
+		return rateOfComment;
+	}
+
+	public void setRateOfComment(int rateOfComment) {
+		this.rateOfComment = rateOfComment;
+	}
+	
+	
+	
+	
+	
+}
