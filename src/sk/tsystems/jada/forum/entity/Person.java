90efe986316156e1dc76733536c3b56fffe667aa
Lukas Straka <lstraka91@gmail.com>
2016-08-22 11:14:12 +0200

    updated by fixed some javadoc warning

--- src/sk/tsystems/jada/forum/entity/Person.java
+++ src/sk/tsystems/jada/forum/entity/Person.java
@@ -87,66 +87,154 @@ public class Person {
 	 ********************************** Getters and setters for fields **************************************
 	 ********************************************************************************************************/
 
+	/**
+	 * return id of Person
+	 * 
+	 * @return id value of Person
+	 */
 	public int getIdPerson() {
 		return idPerson;
 	}
 
+	/**
+	 * set person value of ident
+	 * 
+	 * @param idPerson
+	 *            person value of id
+	 */
 	public void setIdPerson(int idPerson) {
 		this.idPerson = idPerson;
 	}
 
+	/**
+	 * return person userName
+	 * 
+	 * @return String of person username
+	 */
 	public String getPersonName() {
 		return personName;
 	}
 
+	/**
+	 * set the new person userName
+	 * 
+	 * @param personName
+	 *            String of new person name to Person object
+	 */
 	public void setPersonName(String personName) {
 		this.personName = personName;
 	}
 
+	/**
+	 * return password of Person
+	 * 
+	 * @return password of Person
+	 */
 	public String getPassword() {
 		return password;
 	}
 
+	/**
+	 * set the new password for Person
+	 * 
+	 * @param password
+	 *            new password for Person object
+	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
+	/**
+	 * return Persons email address
+	 * 
+	 * @return String representation of email address
+	 */
 	public String getEmail() {
 		return email;
 	}
 
+	/**
+	 * set new email address to Person
+	 * 
+	 * @param email
+	 *            string representation of new email address
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
+	/**
+	 * returns date of birthday
+	 * 
+	 * @return date of Person birthday
+	 */
 	public Date getBirthday() {
 		return birthday;
 	}
 
+	/**
+	 * set Person birthday
+	 * 
+	 * @param birthday
+	 *            new Date for persons birthday
+	 */
 	public void setBirthday(Date birthday) {
 		this.birthday = birthday;
 	}
 
+	/**
+	 * return date of registration user
+	 * 
+	 * @return date of registration user
+	 */
 	public Date getRegistrationDate() {
 		return registrationDate;
 	}
 
+	/**
+	 * set new Registration date of Person
+	 * 
+	 * @param registrationDate
+	 *            new Date of registration
+	 */
 	public void setRegistrationDate(Date registrationDate) {
 		this.registrationDate = registrationDate;
 	}
 
+	/**
+	 * boolean representation if Person is activated or no
+	 * 
+	 * @return true or false if Person is activated
+	 */
 	public boolean isActive() {
 		return active;
 	}
 
+	/**
+	 * set activate property
+	 * 
+	 * @param active
+	 *            boolean representation if person is or no activated
+	 */
 	public void setActive(boolean active) {
 		this.active = active;
 	}
 
+	/**
+	 * return full name of Person
+	 * 
+	 * @return full name of Person
+	 */
 	public String getFullName() {
 		return fullName;
 	}
 
+	/**
+	 * set new fullName of person
+	 * 
+	 * @param fullName
+	 *            new full name for Person
+	 */
 	public void setFullName(String fullName) {
 		this.fullName = fullName;
 	}
